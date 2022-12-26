4f4303a1b94783954f312de1923ec6ef6f856970
kostenkoi24 <kostenko.i@gmail.com>
2022-12-26 13:35:10 +0200

    Homework8

---
+++ Homework8_3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Homework8_3
+{
+
+    enum Positions
+    {
+        Director = 2,
+        Manager = 5,
+        Buhgalter = 8,
+        Security = 16
+    }
+
+    class Account
+    {
+
+        public static bool AskForBonus(Positions worker, int hours)
+        {
+            if (hours > (int)worker)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Positions p = Positions.Director;
+            int hours = 6;
+
+            Console.WriteLine("Position: {0}, Work hours: {1}, Norma: {2}, Bonus {3}", p, hours, ((int)p), Account.AskForBonus(p, hours));
+
+            p = Positions.Security;
+            hours = 15;
+            Console.WriteLine("Position: {0}, Work hours: {1}, Norma: {2}, Bonus {3}", p, hours, ((int)p), Account.AskForBonus(p, hours));
+
+
+            p = Positions.Buhgalter;
+            hours = 9;
+            Console.WriteLine("Position: {0}, Work hours: {1}, Norma: {2}, Bonus {3}", p, hours, ((int)p), Account.AskForBonus(p, hours));
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework8_5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Homework8_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("Введіть дату свого народження у форматі DD/MM/YYYY:");
+
+            string userInput = Console.ReadLine();
+
+
+
+            try
+            {
+                DateTime birthday = DateTime.Parse(userInput);
+
+                string d = birthday.Day.ToString();
+                string m = birthday.Month.ToString();
+                string y = DateTime.Now.AddYears(1).Year.ToString();
+
+                DateTime nextBirthday = DateTime.Parse(d + "/" + m + "/" + y);
+
+
+
+
+                Console.WriteLine($"Yor bithday: {birthday.ToString("d")} \nNext birthday: {nextBirthday.ToString("d")} \nWait days: {(nextBirthday - DateTime.Now).Days}");
+
+                
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("{0} is not in the correct format.", userInput);
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
